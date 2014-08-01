c1aa21a752c6a9a4463f32517833832463f9c072
maxingsong <maxingsong@xunlei.com>
2014-08-01 13:46:58 +0800

    add some test for protobuf

--- algorithm/data_structure/circle_list.cpp
+++ algorithm/data_structure/circle_list.cpp
@@ -1,7 +1,7 @@
 bool has_circle(list *head)
 {
     bool result = false;
-    int j = 0;
+    int j = 1;
 
     list *fast, *slow;
 
@@ -20,7 +20,7 @@ bool has_circle(list *head)
     // }
 
     for(fast = slow = head; fast != NULL; fast = fast->next) {
-        if (j & 2 == 0) {
+        if (j & 1 == 0) {
             if (fast == slow) {
                 result = true;
                 break;
--- cplus/protobuf/demo01/person.pb
+++
@@ -1,1 +0,0 @@
-{Bobbob@gmail.com
